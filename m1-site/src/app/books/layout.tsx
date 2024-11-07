385e268991b5d901036917889b904c88759490ce
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-07 13:43:18 +0100

    feat: Create Books Page

---
+++ m1-site/src/app/books/layout.tsx
@@ -0,0 +1,8 @@
+// src/app/books/layout.tsx
+export default function BooksLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
---
+++ m1-site/src/app/books/page.tsx
@@ -0,0 +1,3 @@
+export default function BooksPage() {
+    return <h1>Liste des livres</h1>;
+}
