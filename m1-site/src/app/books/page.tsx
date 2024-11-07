093251bc47a83b76b503874dc21967a448136765
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-07 13:59:28 +0100

    feat: add Breadcrumb to Books page

--- m1-site/src/app/books/page.tsx
+++ m1-site/src/app/books/page.tsx
@@ -1,3 +1,12 @@
+import Breadcrumb from "../../components/Breadcrumb";
+
 export default function BooksPage() {
-    return <h1>Liste des livres</h1>;
+    return(
+        <div>
+            <Breadcrumb links={[
+                { href: '/books', label: 'Liste des livres' }
+            ]} />
+            <h1>Liste des livres</h1>;
+        </div>
+    )
 }
