5d3c37fb18e61d29e08706bd078bd210745066c3
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-07 14:47:09 +0100

    style: enhance Navbar styling with Tailwind CSS

--- m1-site/src/components/Navbar.tsx
+++ m1-site/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 const Navbar=()=>(
-    <nav>
-        <ul>
-            <li><Link href={"/"}>Accueil</Link></li>
-            <li><Link href={"/books"}>Livres</Link></li>
-            <li><Link href={"/authors"}>Auteurs</Link></li>
+    <nav className="bg-gray-800 p-4">
+        <ul className="flex justify-center space-x-8">
+            <li><Link href={"/"} className="text-white hover:text-gray-300">Accueil</Link></li>
+            <li><Link href={"/books"} className="text-white hover:text-gray-300">Livres</Link></li>
+            <li><Link href={"/authors"} className="text-white hover:text-gray-300">Auteurs</Link></li>
         </ul>
     </nav>
 );
