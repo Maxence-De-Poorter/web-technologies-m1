58a7cf8055113e4585712b7e6235b6881cf76e01
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-07 14:10:41 +0100

    feat: add Breadcrumb to Author Page

--- m1-site/src/app/authors/page.tsx
+++ m1-site/src/app/authors/page.tsx
@@ -1,3 +1,12 @@
+import Breadcrumb from "../../components/Breadcrumb";
+
 export default function AuthorsPage() {
-    return <h1>Liste des auteurs</h1>;
+    return (
+        <div>
+            <Breadcrumb links={[
+                {href: '/authors', label: 'Liste des auteurs'}
+            ]}/>
+            <h1>Liste des auteurs</h1>
+        </div>
+    )
 }
