29c2ee5579fc5a5314aab15bc20a2537c5e98e14
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-07 14:09:07 +0100

    chore: remove unnecessary comments

--- m1-site/src/app/authors/Layout.tsx
+++ m1-site/src/app/authors/Layout.tsx
@@ -1,4 +1,3 @@
-// src/app/books/layout.tsx
 export default function AuthorsLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
--- m1-site/src/app/books/layout.tsx
+++ m1-site/src/app/books/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/books/layout.tsx
 export default function BooksLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
