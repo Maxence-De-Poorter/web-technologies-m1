9143da89a4d11cb68134e416c116d4703ea5cfa3
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-09 11:56:04 +0100

    feat: add books page with mock data for testing

--- m1-site/src/app/books/page.tsx
+++ m1-site/src/app/books/page.tsx
@@ -1,14 +1,45 @@
 import Breadcrumb from "../../components/Breadcrumb";
 import PageTitle from "../../components/PageTitle";
 
+function fetchBooks() {
+    //A remplacer par une vrai requete vers le back
+    return [
+        { id: 1, title: 'Book One', author: 'Author A', publishedDate: '2023' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+        { id: 2, title: 'Book Two', author: 'Author B', publishedDate: '2021' },
+    ];
+}
+
 export default function BooksPage() {
+    const books = fetchBooks();
+
     return(
         <div>
             <PageTitle>Liste des livres</PageTitle>
             <Breadcrumb links={[
-                { href: '/', label: 'Accueil' },
-                { href: '/books', label: 'Liste des livres' }
-            ]} />
+                {href: '/', label: 'Accueil'},
+                {href: '/books', label: 'Liste des livres'}
+            ]}/>
+
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>
+                        <h2>{book.title}</h2>
+                        <p>Auteur : {book.author}</p>
+                        <p>Date de publication : {book.publishedDate}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
