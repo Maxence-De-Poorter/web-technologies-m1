82201ee0966ba0b00e456c0272c12a81eada6350
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-13 16:20:33 +0100

    feat: add import "React"

--- m1-site/src/app/layout.tsx
+++ m1-site/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import '../styles/globals.css';
+import React from "react";
 
 export const metadata = {
   title: 'Next.js',
