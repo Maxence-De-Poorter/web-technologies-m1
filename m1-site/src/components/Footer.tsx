0090e0d7a996fa5f56a09796eed8a02cc9851baf
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-13 16:15:30 +0100

    feat: Add translated and documented Footer component

--- m1-site/src/components/Footer.tsx
+++ m1-site/src/components/Footer.tsx
@@ -1,13 +1,8 @@
-import Link from "next/link";
-
+// Footer component displaying navigation links and copyright information
 const Footer = () => (
     <footer className="bg-gray-800 p-4 mt-8">
-        <ul className="flex justify-center space-x-8">
-            <li><Link href={"/"} className="text-white hover:text-gray-300">Accueil</Link></li>
-            <li><Link href={"/books"} className="text-white hover:text-gray-300">Livres</Link></li>
-            <li><Link href={"/authors"} className="text-white hover:text-gray-300">Auteurs</Link></li>
-        </ul>
-        <p className="text-center text-white mt-4">&copy; 2024 M1 - Technologies Web. Tous droits réservés.</p>
+        {/* Copyright information */}
+        <p className="text-center text-white mt-4">&copy; 2024 M1 - Web Technologies. All rights reserved.</p>
     </footer>
 );
 
