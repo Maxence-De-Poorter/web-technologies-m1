b1774d39a8f14a3808cede06a084ed302afaf4e5
Maxence DE POORTER <90178284+Maxence-De-Poorter@users.noreply.github.com>
2024-11-13 16:17:17 +0100

    feat: add Translate Navbar component to English with comments

--- m1-site/src/components/Navbar.tsx
+++ m1-site/src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 
-const Navbar=()=>(
+// Navbar component displaying a navigation bar with links to Home, Books, and Authors pages
+const Navbar = () => (
     <nav className="bg-gray-800 p-4">
         <ul className="flex justify-center space-x-8">
-            <li><Link href={"/"} className="text-white hover:text-gray-300">Accueil</Link></li>
-            <li><Link href={"/books"} className="text-white hover:text-gray-300">Livres</Link></li>
-            <li><Link href={"/authors"} className="text-white hover:text-gray-300">Auteurs</Link></li>
+            {/* Link to the Home page */}
+            <li><Link href={"/"} className="text-white hover:text-gray-300">Home</Link></li>
+            {/* Link to the Books page */}
+            <li><Link href={"/books"} className="text-white hover:text-gray-300">Books</Link></li>
+            {/* Link to the Authors page */}
+            <li><Link href={"/authors"} className="text-white hover:text-gray-300">Authors</Link></li>
         </ul>
     </nav>
 );
